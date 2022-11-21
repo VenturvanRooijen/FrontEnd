49f0026d2b6335c317dea5a127ce5349c0d25a1c
Ventur van Rooijen <99063409@mydavinci.nl>
2022-11-21 09:56:15 +0100

    Add all heroes

--- script.js
+++ script.js
@@ -2,13 +2,46 @@
 const heroName = [
     "Ana",
     "Ashe",
-    "Baptiste"
+    "Baptiste",
+    "Bastion",
+    "Brigitte",
+    "Cassidy",
+    "D.Va",
+    "Doomfist",
+    "Echo",
+    "Genji",
+    "Hanzo",
+    "Junkrat",
+    "Kiriko",
+    "Lucio",
+    "Mei",
+    "Mercy",
+    "Moira",
+    "Orisa",
+    "Pharah",
+    "Queen",
+    "Reaper",
+    "Reinhardt",
+    "Roadhog",
+    "Sigma",
+    "Sojourn",
+    "Soldier",
+    "Sombra",
+    "Symmetra",
+    "Torbjorn",
+    "Tracer",
+    "Widowmaker",
+    "Winston",
+    "Zarya",
+    "Zenyatta"
 ];
 
 
 
 const randomHero = heroName[Math.floor(Math.random() * heroName.length)];
+console.log(randomHero)
 
+const image = document.getElementsByClassName('hero');
 
-document.getElementById("hero").src = "images/" + randomHero + ".png";
+image[0].src = "images/" + randomHero + ".png";
 
