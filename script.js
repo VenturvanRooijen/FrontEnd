041962522a77aebcaf185ca2b66ed5fb8e915b71
Ventur van Rooijen <99063409@mydavinci.nl>
2022-11-23 13:11:21 +0100

    Complete Code Rework

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 
-const heroName = [
-    "Ana",
-    "Ashe",
-    "Baptiste",
-    "Bastion",
-    "Brigitte",
-    "Cassidy",
-    "D.Va",
-    "Doomfist",
-    "Echo",
-    "Genji",
-    "Hanzo",
-    "Junkrat",
-    "Kiriko",
-    "Lucio",
-    "Mei",
-    "Mercy",
-    "Moira",
-    "Orisa",
-    "Pharah",
-    "Queen",
-    "Reaper",
-    "Reinhardt",
-    "Roadhog",
-    "Sigma",
-    "Sojourn",
-    "Soldier",
-    "Sombra",
-    "Symmetra",
-    "Torbjorn",
-    "Tracer",
-    "Widowmaker",
-    "Winston",
-    "Zarya",
-    "Zenyatta"
+
+var heroName = [{
+    text : "Ana",
+    image : "images/Ana.png"
+},
+{
+    text : "Ashe",
+    image : "images/Ashe.png"
+},
+{
+    text : "Baptiste",
+    image : "images/Baptiste.png"
+},
+{
+    text : "Bastion",
+    image : "images/Bastion.png"
+},
+{
+    text : "Brigitte",
+    image : "images/Brigitte.png"
+},
+{
+    text : "Cassidy",
+    image : "images/Cassidy.png"
+},
+{
+    text : "D.Va",
+    image : "images/D.Va.png"
+},
+{
+    text : "Doomfist",
+    image : "images/Doomfist.png"
+},
+{
+    text : "Echo",
+    image : "images/Echo.png"
+},
+{
+    text : "Genji",
+    image : "images/Genji.png"
+},
+{
+    text : "Hanzo",
+    image : "images/Ana.png"
+}
 ];
 
 
@@ -41,7 +51,11 @@ const heroName = [
 const randomHero = heroName[Math.floor(Math.random() * heroName.length)];
 console.log(randomHero)
 
-const image = document.getElementsByClassName('hero');
+var container = document.getElementById("image-container");
+var img = document.createElement('img');
+img.src = randomHero.image;
+img.className = "w3-round";
+container.appendChild(img);
 
-image[0].src = "images/" + randomHero + ".png";
+
 
