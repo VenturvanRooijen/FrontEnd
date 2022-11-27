7cc884ddf7c231f77e8ea7460630d656172f085f
Ventur van Rooijen <99063409@mydavinci.nl>
2022-11-27 12:42:33 +0100

    Add Amount of Heroes to be Displayed, too Many/Little heroes

--- script.js
+++ script.js
@@ -43,19 +43,126 @@ var heroName = [{
 {
     text : "Hanzo",
     image : "images/Ana.png"
+},
+{
+    text : "Junkrat",
+    image : "images/Junkrat.png"
+},
+{
+    text : "Kiriko",
+    image : "images/Kiriko.png"
+},
+{
+    text : "Lucio",
+    image : "images/Lucio.png"
+},
+{
+    text : "Mei",
+    image : "images/Mei.png"
+},
+{
+    text : "Mercy",
+    image : "images/Mercy.png"
+},
+{
+    text : "Moira",
+    image : "images/Moira.png"
+},
+{
+    text : "Orisa",
+    image : "images/Orisa.png"
+},
+{
+    text : "Pharah",
+    image : "images/Pharah.png"
+},
+{
+    text : "Queen",
+    image : "images/Queen.png"
+},
+{
+    text : "Reaper",
+    image : "images/Reaper.png"
+},
+{
+    text : "Reinhardt",
+    image : "images/Reinhardt.png"
+},
+{
+    text : "Roadhog",
+    image : "images/Roadhog.png"
+},
+{
+    text : "Sigma",
+    image : "images/Sigma.png"
+},
+{
+    text : "Sojourn",
+    image : "images/Sojourn.png"
+},
+{
+    text : "Soldier",
+    image : "images/Soldier.png"
+},
+{
+    text : "Sombra",
+    image : "images/Sombra.png"
+},
+{
+    text : "Symmetra",
+    image : "images/Symmetra.png"
+},
+{
+    text : "Torbjorm",
+    image : "images/Torbjorn.png"
+},
+{
+    text : "Tracer",
+    image : "images/Tracer.png"
+},
+{
+    text : "Widowmaker",
+    image : "images/Widowmaker.png"
+},
+{
+    text : "Winston",
+    image : "images/Winston.png"
+},
+{
+    text : "Zarya",
+    image : "images/Zarya.png"
+},
+{
+    text : "Zenyatta",
+    image : "images/Zenyatta.png"
 }
 ];
 
+var heroCount = 5;
 
+if(heroCount <= 36 && heroCount >= 5) {
+
+for(let i = 0; i < heroCount; i++) {
 
 const randomHero = heroName[Math.floor(Math.random() * heroName.length)];
-console.log(randomHero)
+console.log(randomHero);
+
+
 
 var container = document.getElementById("image-container");
 var img = document.createElement('img');
 img.src = randomHero.image;
-img.className = "w3-round";
+img.style = "border:3px solid orange; border-radius:50px; width:100px";
 container.appendChild(img);
+
+};
+
+}
+else {
+    document.getElementById("teVeel").textContent += "Aantal heroes moet tussen de 5 en 36 zijn";
+    document.getElementById("teVeel").style = "display: block;"
+
+}
 
 
 
