8ab2a090847e7dda3a19ebe27a5f0147a6ccb37c
Ventur van Rooijen <99063409@mydavinci.nl>
2022-11-27 14:15:46 +0100

    Redo Image Generation, Add Duplicate Check

--- script.js
+++ script.js
@@ -1,168 +1,200 @@
 
 
-var heroName = [{
+var images = [{
     text : "Ana",
-    image : "images/Ana.png"
+    image : "images/Ana.png",
+    used : false
 },
 {
     text : "Ashe",
-    image : "images/Ashe.png"
+    image : "images/Ashe.png",
+    used : false
 },
 {
     text : "Baptiste",
-    image : "images/Baptiste.png"
+    image : "images/Baptiste.png",
+    used : false
 },
 {
     text : "Bastion",
-    image : "images/Bastion.png"
+    image : "images/Bastion.png",
+    used : false
 },
 {
     text : "Brigitte",
-    image : "images/Brigitte.png"
+    image : "images/Brigitte.png",
+    used : false
 },
 {
     text : "Cassidy",
-    image : "images/Cassidy.png"
+    image : "images/Cassidy.png",
+    used : false
 },
 {
     text : "D.Va",
-    image : "images/D.Va.png"
+    image : "images/D.Va.png",
+    used : false
 },
 {
     text : "Doomfist",
-    image : "images/Doomfist.png"
+    image : "images/Doomfist.png",
+    used : false
 },
 {
     text : "Echo",
-    image : "images/Echo.png"
+    image : "images/Echo.png",
+    used : false
 },
 {
     text : "Genji",
-    image : "images/Genji.png"
+    image : "images/Genji.png",
+    used : false
 },
 {
     text : "Hanzo",
-    image : "images/Ana.png"
+    image : "images/Ana.png",
+    used : false
 },
 {
     text : "Junkrat",
-    image : "images/Junkrat.png"
+    image : "images/Junkrat.png",
+    used : false
 },
 {
     text : "Kiriko",
-    image : "images/Kiriko.png"
+    image : "images/Kiriko.png",
+    used : false
 },
 {
     text : "Lucio",
-    image : "images/Lucio.png"
+    image : "images/Lucio.png",
+    used : false
 },
 {
     text : "Mei",
-    image : "images/Mei.png"
+    image : "images/Mei.png",
+    used : false
 },
 {
     text : "Mercy",
-    image : "images/Mercy.png"
+    image : "images/Mercy.png",
+    used : false
 },
 {
     text : "Moira",
-    image : "images/Moira.png"
+    image : "images/Moira.png",
+    used : false
 },
 {
     text : "Orisa",
-    image : "images/Orisa.png"
+    image : "images/Orisa.png",
+    used : false
 },
 {
     text : "Pharah",
-    image : "images/Pharah.png"
+    image : "images/Pharah.png",
+    used : false
 },
 {
     text : "Queen",
-    image : "images/Queen.png"
+    image : "images/Queen.png",
+    used : false
 },
 {
     text : "Reaper",
-    image : "images/Reaper.png"
+    image : "images/Reaper.png",
+    used : false
 },
 {
     text : "Reinhardt",
-    image : "images/Reinhardt.png"
+    image : "images/Reinhardt.png",
+    used : false
 },
 {
     text : "Roadhog",
-    image : "images/Roadhog.png"
+    image : "images/Roadhog.png",
+    used : false
 },
 {
     text : "Sigma",
-    image : "images/Sigma.png"
+    image : "images/Sigma.png",
+    used : false
 },
 {
     text : "Sojourn",
-    image : "images/Sojourn.png"
+    image : "images/Sojourn.png",
+    used : false
 },
 {
     text : "Soldier",
-    image : "images/Soldier.png"
+    image : "images/Soldier.png",
+    used : false
 },
 {
     text : "Sombra",
-    image : "images/Sombra.png"
+    image : "images/Sombra.png",
+    used : false
 },
 {
     text : "Symmetra",
-    image : "images/Symmetra.png"
+    image : "images/Symmetra.png",
+    used : false
 },
 {
     text : "Torbjorm",
-    image : "images/Torbjorn.png"
+    image : "images/Torbjorn.png",
+    used : false
 },
 {
     text : "Tracer",
-    image : "images/Tracer.png"
+    image : "images/Tracer.png",
+    used : false
 },
 {
     text : "Widowmaker",
-    image : "images/Widowmaker.png"
+    image : "images/Widowmaker.png",
+    used : false
 },
 {
     text : "Winston",
-    image : "images/Winston.png"
+    image : "images/Winston.png",
+    used : false
 },
 {
     text : "Zarya",
-    image : "images/Zarya.png"
+    image : "images/Zarya.png",
+    used : false
 },
 {
     text : "Zenyatta",
-    image : "images/Zenyatta.png"
+    image : "images/Zenyatta.png",
+    used : false
 }
 ];
 
-var heroCount = 5;
+var counter = 0; var randomIndex=0; 
+ 
 
-if(heroCount <= 36 && heroCount >= 5) {
+var displayImages=34; 
+ if(displayImages >= 5 && displayImages <= 34){
+while(counter !== displayImages){ 
+	randomIndex = Math.floor(Math.random()*images.length); 
+	if(images[randomIndex]["used"] === false){ 
+		images[randomIndex]["used"] = true; 
+ 
+		var container = document.getElementById("image-container");
+        var img = document.createElement('img');
+        img.src = images[randomIndex]["image"];
+        img.style = "border:3px solid orange; border-radius:50px; width:100px";
+        container.appendChild(img);
+        console.log(images)
+		 
+ 
+		counter++; 
+	} 
+} 
 
-for(let i = 0; i < heroCount; i++) {
-
-const randomHero = heroName[Math.floor(Math.random() * heroName.length)];
-console.log(randomHero);
-
-
-
-var container = document.getElementById("image-container");
-var img = document.createElement('img');
-img.src = randomHero.image;
-img.style = "border:3px solid orange; border-radius:50px; width:100px";
-container.appendChild(img);
-
-};
-
-}
-else {
-    document.getElementById("teVeel").textContent += "Aantal heroes moet tussen de 5 en 36 zijn";
+} else{
+    document.getElementById("teVeel").textContent += "Aantal heroes moet tussen de 5 en 34 zijn";
     document.getElementById("teVeel").style = "display: block;"
-
-}
-
-
-
+};
